f9156a085f733ff2dcc86b65c74405c52763ee88
Carolina Castioni <carolinapcastioni@gmail.com>
2020-05-10 18:41:00 +0200

    done

--- SRC/codes.py
+++ SRC/codes.py
@@ -1,7 +1,10 @@
-import pandas as pd 
+import pandas as pd
+
 
 def open_file(file):
     df = pd.read_csv(file, error_bad_lines=False)
     return df
 
-    
+def get_len(pred):
+    return len(pred)
+
